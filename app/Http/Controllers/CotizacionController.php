a80bcbff61e95d5c4d53b8eb1beba64588770dc4
Saygon96 <98422604+Saygon96@users.noreply.github.com>
2024-04-10 11:11:21 -0600

    q

---
+++ app/Http/Controllers/CotizacionController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Middleware\NoCache;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
+class CotizacionController extends Controller
+{
+    public function index ()
+    {
+        $title = 'Cotización';
+
+        return view('/cotizacion', [
+            'title' => $title,
+        ]);
+    }
+    public function __construct()
+    {
+        //$this->middleware(NoCache::class);
+    }
+
+    public function crear()
+    {
+        // Lógica para crear una nueva cotización
+    }
+
+    public function read()
+    {
+        // Lógica para leer una cotización existente
+    }
+
+    public function update()
+    {
+        // Lógica para actualizar una cotización existente
+    }
+
+    public function delete($id)
+    {
+        // Lógica para eliminar una cotización existente
+    }
+
+}
