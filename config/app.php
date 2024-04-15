3253eda1ae8b972581d04a402649a3be8578399b
Saygon96 <98422604+Saygon96@users.noreply.github.com>
2024-04-15 10:12:59 -0600

    update

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://cotizador.selfish.com.mx'),
 
     'asset_url' => env('ASSET_URL'),
 
